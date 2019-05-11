90aa7d7e4d4759aecd627956a504d47a7c9fef46
Diego Lopes <diego-chez@hotmail.com>
2019-05-11 12:38:25 -0300

    cria o componente header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "./components/HeaderComponent";
+
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/HeaderComponent/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default props => (
+  <header className="main-header">
+    <p>
+      <span className="main-header__js">JS</span>Hunt
+    </p>
+  </header>
+);
