3412b0083cdc9ff73ac55f31f91e681cab67c117
Diego Lopes <diego-chez@hotmail.com>
2019-05-12 08:31:52 -0300

    cria loading para as paginas

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import Loading from "../../components/LoadingComponent";
 import { Link } from "react-router-dom";
 
 import "./styles.css";
@@ -8,7 +9,8 @@ export default class Main extends Component {
   state = {
     products: [],
     productsInfo: {},
-    page: 1
+    page: 1,
+    loading: true
   };
 
   componentDidMount() {
@@ -18,7 +20,7 @@ export default class Main extends Component {
   loadProducts = async (page = 1) => {
     const { data } = await api.get(`/products?page=${page}`);
     const { docs, ...productsInfo } = data;
-    this.setState({ products: docs, productsInfo, page });
+    this.setState({ products: docs, productsInfo, page, loading: false });
   };
 
   prevPage = () => {
@@ -38,30 +40,36 @@ export default class Main extends Component {
   };
 
   render() {
-    const { products, page, productsInfo } = this.state;
+    const { products, page, productsInfo, loading } = this.state;
 
     return (
-      <div className="products-list">
-        {products.map(product => (
-          <article key={product._id}>
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
+      <React.Fragment>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="products-list">
+            {products.map(product => (
+              <article key={product._id}>
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
 
-            <Link to={`/products/${product._id}`}>Acessar</Link>
-          </article>
-        ))}
-        <div className="actions">
-          <button disabled={page === 1} onClick={this.prevPage}>
-            Anterios
-          </button>
-          <button
-            disabled={page === productsInfo.pages}
-            onClick={this.nextPage}
-          >
-            Próxima
-          </button>
-        </div>
-      </div>
+                <Link to={`/products/${product._id}`}>Acessar</Link>
+              </article>
+            ))}
+            <div className="actions">
+              <button disabled={page === 1} onClick={this.prevPage}>
+                Anterios
+              </button>
+              <button
+                disabled={page === productsInfo.pages}
+                onClick={this.nextPage}
+              >
+                Próxima
+              </button>
+            </div>
+          </div>
+        )}
+      </React.Fragment>
     );
   }
 }
